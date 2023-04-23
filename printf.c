d6a0cddc6f0a754818f622080871ce14bb6024f1
mina-william-23 <capeandroid50@gmail.com>
2023-04-23 14:32:36 -0700

    add task 0

---
+++ printf.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _printf - mimic printf from stdio to standard output
+ * @format: input to print
+ * Return: printed bytes
+ */
+int _printf(const char *format, ...)
+{
+	int printed;
+	va_list args;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	printed = handle_format(args, format);
+
+	va_end(args);
+
+	return (length);
+}
