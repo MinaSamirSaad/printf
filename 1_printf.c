ed41130b0ebb60c812763c327407ce641b5449b2
mina-william-23 <capeandroid50@gmail.com>
2023-04-22 17:04:21 -0700

    edit 1_printf.c

--- 1_printf.c
+++ 1_printf.c
@@ -1,5 +1,22 @@
 #include "main.h"
 /**
+ * printstr - print string
+ * @s: string
+ * Return: number of printed
+*/
+int printstr(char *s)
+{
+	int printed = 0;
+
+	while (s && *s)
+	{
+		write(1, s, 1);
+		++s;
+		++printed;
+	}
+	return (printed);
+}
+/**
  * _printf - printf mimic
  * @format: string
  * Return: number of printed
@@ -7,54 +24,39 @@
 int _printf(const char *format, ...)
 {
 	int res = 0;
+	char let, *st;
 	va_list args;
-	char let;
-	char *st;
 
 	if (!format)
 		return (res);
-
 	va_start(args, format);
 	while (*format)
-    {
-    	if (*format == '%')
-    	{
-    		++format;
-    		if (!format)
-    			break;
-    		switch (*format)
-    		{
-	    		case 'c':
-	    			let = va_arg(args, int);
-	    			write(1, &let, 1);
-	    			break;
-	    		case 's':
-	    			st = va_arg(args, char *);
-	    			res--;
-	    			if (!st)
-	    				break;
-	    			while (*st)
-	    			{
-	    				write(1, st, 1);
-	    				++st;
-	    				++res;
-	    			}
-	    			break;
-	    		case '%':
-	    			write(1, format, 1);
-	    			break;
-	    		default:
-	    			write(1, format, 1);
-    		}
-    	}
-    	else
-    	{
-    		write(1, format, 1);
-    	}
-    	++format;
-    	++res;
+	{
+		if (*format == '%')
+		{
+			++format;
+			if (!format)
+				break;
+			switch (*format)
+			{
+				case 'c':
+					let = va_arg(args, int);
+					write(1, &let, 1);
+					break;
+				case 's':
+					st = va_arg(args, char *);
+					res--;
+					res += printstr(st);
+					break;
+				default:
+					write(1, format, 1);
+			}
+		}
+		else
+			write(1, format, 1);
+		++format;
+		++res;
 	}
 	va_end(args);
-
 	return (res);
 }
