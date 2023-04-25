01913b838de6ae2e07b7ef25851ae2a598f4c586
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-04-25 16:29:36 +0200

    update custom string

--- main.h
+++ main.h
@@ -13,5 +13,6 @@ int print_unsigned_integer(va_list args, char tag);
 int print_binary(va_list args);
 int print_address(va_list args);
 int print_custom_str(va_list args);
+int custom_str_helper(unsigned char *str);
 
 #endif
